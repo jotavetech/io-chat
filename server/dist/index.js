cc09a2178ced189c9380330e131ea2744a18ece5
jotavetech <ojoaogugel@gmail.com>
2024-03-15 09:48:27 -0300

    io setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,1 +1,19 @@
-"use strict";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import express from "express";
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to socket.io</h1>");
+});
+io.on("connection", (socket) => {
+    console.log(socket);
+});
+server.listen(3000, () => {
+    console.log("Server is running on localhost:3000");
+});
